b84aa3c7b260457465b816e92593eea1a1b66923
Shamsidddinshoma <qurbonovshamsiddin0423@gmail.com>
2023-01-26 16:59:16 +0500

    dastur pensiya

---
+++ main.js
@@ -0,0 +1,14 @@
+// {
+//     function pensiya(yosh) {
+//         if (yosh > 50 ) {
+//             console.log('pensiya yoshi');
+//         }else{
+//             console.log('siz pensiya yoshida emassiz');
+//         }
+//     }
+//     pensiya(51)
+// }
+
+
+
+
